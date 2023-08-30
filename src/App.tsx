f6ae119b1a3ed94d7a1b65b5b0ab4caca2bf87f9
HabibMollah <hrmollah2002@gmail.com>
2023-08-30 17:27:28 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <main>
+      <div id="break-label">Break Length</div>
+      <div id="session-label">Session Length</div>
+    </main>
+  );
+}
