e68871d96c39253c6902dc67eee83ffc0972c8fb
HabibMollah <hrmollah2002@gmail.com>
2023-09-01 15:46:40 +0600

    fully functional timer with custom session length

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,29 @@ import './App.css';
 import msToMMSSConverter from './msToMMSSConverter';
 
 export default function App() {
-  const [time, setTime] = useState(25 * 60 * 1000);
+  const [sessionLength, setSessionLength] = useState(25 * 60 * 1000);
+  const [breakLength, setBreakLength] = useState(5 * 60 * 1000);
+  const [time, setTime] = useState(sessionLength);
   const [isStarted, setIsStarted] = useState(false);
 
   useEffect(() => {
-    if (isStarted) {
-      setTimeout(() => {
+    if (!isStarted)
+      return () => {
+        return;
+      };
+
+    const timer =
+      time > 0 &&
+      setInterval(() => {
         setTime(time - 1000);
       }, 1000);
-    }
+
+    return () => timer && clearInterval(timer);
   });
+
+  useEffect(() => {
+    setTime(sessionLength);
+  }, [sessionLength]);
 
   return (
     <main>
@@ -30,7 +43,12 @@ export default function App() {
         </label>
         <div className="flex">
           <button
-            onClick={() => setTime(time - 60 * 1000)}
+            onClick={() => {
+              if (!isStarted) {
+                if (sessionLength > 60 * 1000 && !isStarted)
+                  setSessionLength(sessionLength - 60 * 1000);
+              }
+            }}
             id="session-decrement">
             -
           </button>
@@ -38,10 +56,12 @@ export default function App() {
             disabled
             type="number"
             id="session-length"
-            value={time / 1000 / 60}
+            value={Math.floor(sessionLength / 1000 / 60)}
           />
           <button
-            onClick={() => setTime(time + 60 * 1000)}
+            onClick={() => {
+              if (!isStarted) setSessionLength(sessionLength + 60 * 1000);
+            }}
             id="session-increment">
             +
           </button>
@@ -57,7 +77,13 @@ export default function App() {
         <button onClick={() => setIsStarted(!isStarted)} id="start_stop">
           start | stop
         </button>
-        <button onClick={() => setTime(25 * 60 * 1000)} id="reset">
+        <button
+          onClick={() => {
+            setIsStarted(false);
+            setSessionLength(25 * 60 * 1000);
+            setTime(25 * 60 * 1000);
+          }}
+          id="reset">
           reset
         </button>
       </section>
